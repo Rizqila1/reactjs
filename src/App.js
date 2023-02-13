db3b5fd6a9ba2d37aaff1df13a2626024c72fc80
Rizqila1 <ikilatr@gmail.com>
2023-02-13 16:47:48 +0700

    installed library

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Rizqila Testing 4</p>
+        <p>Rizqila Testing 5</p>
         <a
           className="App-link"
           href="https://reactjs.org"
