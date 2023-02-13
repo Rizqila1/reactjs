062a76d9878d963575622afe5467e14c5273274d
Rizqila1 <ikilatr@gmail.com>
2023-02-13 15:58:00 +0700

    test 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Rizqila Testing Deploy</p>
+        <p>Rizqila Testing 2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
